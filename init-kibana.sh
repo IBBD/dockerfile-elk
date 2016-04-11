f0996728e6350116a1ac8fcc34ac7732fe25821e
alex cai <caiyingyao@ibbd.net>
2016-04-11 14:44:51 +0800

    init kibana.sh

---
+++ init-kibana.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# 
+
+# logo
+docker cp kibana/logo/favicon.ico ibbd-kibana:/opt/kibana/optimize/bundles/src/ui/public/images/elk.ico 
+docker cp kibana/logo/kibana.svg ibbd-kibana:/opt/kibana/optimize/bundles/src/ui/public/images/kibana.svg
+
+if [ ! -d tmp/ ]; then
+    mkdir tmp
+fi
+
+# title
+docker cp ibbd-kibana:/opt/kibana/src/ui/views/chrome.jade tmp/
+sed -i "s/    title Kibana/    title EyeInsight-火眼洞察/" tmp/chrome.jade
+docker cp tmp/chrome.jade ibbd-kibana:/opt/kibana/src/ui/views/chrome.jade
+
+# 启动信息
+docker cp ibbd-kibana:/opt/kibana/src/ui/views/ui_app.jade tmp/
+sed -i "s/      strong Kibana/      strong EyeInsight/" tmp/ui_app.jade
+docker cp tmp/ui_app.jade ibbd-kibana:/opt/kibana/src/ui/views/ui_app.jade
+
+echo 'Over!!'
