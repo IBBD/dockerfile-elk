959a800d35ed56c059b1455c7d41b8897dd19b22
alex cai <caiyingyao@ibbd.net>
2016-04-23 00:43:57 +0800

    init kibana

--- init-kibana.sh
+++ init-kibana.sh
@@ -70,6 +70,10 @@ str_replace "tmp/kibana.bundle.js" "Ready to get started" "准备开始建立仪
 str_replace "tmp/kibana.bundle.js" "Selected Fields" "选择字段"
 str_replace "tmp/kibana.bundle.js" "Available Fields" "可选择字段"
 str_replace "tmp/kibana.bundle.js" ">Popular<" ">字段列表<"
+str_replace "tmp/kibana.bundle.js" "Select a search source" "选择一个数据源"
+str_replace "tmp/kibana.bundle.js" "From a new search" "选择已有索引作为数据源"
+str_replace "tmp/kibana.bundle.js" "From a saved search" "选择保存好的搜索结果作为数据源"
+str_replace "tmp/kibana.bundle.js" "Select an index pattern" "从下拉框选择一个索引"
 #str_replace "tmp/kibana.bundle.js" "This field is present in your elasticsearch mapping but not in any documents in the search results. You may still be able to visualize or search on it." "这个字段只会在mapping的配置文件中，而不会出现在搜索结果上。不过你依然可以看到它或者搜索它。"
 
 # 修改图表类型
