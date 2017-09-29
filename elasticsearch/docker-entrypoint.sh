cd532be6619063cbf3450b55592ddcfead1022df
alex cai <caiyingyao@ibbd.net>
2017-09-29 18:02:54 +0800

    es user

--- elasticsearch/docker-entrypoint.sh
+++ elasticsearch/docker-entrypoint.sh
@@ -5,13 +5,24 @@
 # Created Time: 2017年09月29日 星期五 15时51分53秒
 set -e
 
-# remove x-pack
-if 
-    elasticsearch-plugin list|grep x-pack;
-then
-    elasticsearch-plugin remove x-pack || true
-else
-    echo "x-pack is not installed."
+# Add elasticsearch as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- elasticsearch "$@"
+fi
+
+# Drop root privileges if we are running elasticsearch
+# allow the container to be started with `--user`
+if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+	# Change the ownership of user-mutable directories to elasticsearch
+	for path in \
+		/usr/share/elasticsearch/data \
+		/usr/share/elasticsearch/logs \
+	; do
+		chown -R elasticsearch:elasticsearch "$path"
+	done
+	
+	set -- gosu elasticsearch "$@"
+	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
 fi
 
 # As argument is not related to elasticsearch,
