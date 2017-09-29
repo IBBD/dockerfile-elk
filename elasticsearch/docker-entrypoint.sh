194eea1dd2cde410a04bb74130e6815d53bfc4d2
alex cai <caiyingyao@ibbd.net>
2017-09-29 15:56:01 +0800

    es new

---
+++ elasticsearch/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 
+# 
+# Author: alex
+# Created Time: 2017年09月29日 星期五 15时51分53秒
+set -e
+
+# remove x-pack
+./bin/elasticsearch-plugin remove x-pack || true
+
+# As argument is not related to elasticsearch,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
+exec "$@"
